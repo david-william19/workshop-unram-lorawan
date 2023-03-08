603b67fe55270b0de3621a49bf39a2672172bad6
David William Da Costa <davidddwlliam@gmail.com>
2023-03-08 09:15:15 +0700

    task for learn

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # antares-workshop-LoRaWAN
 
+halo sobat antares, jadi ini merupakan repository untuk pembelajaran mengenai dashboard IOT menggunakan protokol loraWAN. disini teman-teman perlu melakukan beberapa hal ketika ingin menggunakan aplikasi ini.
+
+## requirement
+sebelum menjalankan aplikasi ini, teman-teman perlu melengkapi beberapa aplikasi berikut
+1. git (optional)
+2. node JS
+3. VS Code (optional, bisa menggunakan IDE lain)
+
+## guide
+1. pertama teman-teman perlu clone repo ini
+```bash
+# script clone ketika sudah mempunyai git
+git clone https://github.com/david-william19/workshop-unram-lorawan.git
+```
+atau teman-teman bisa langsung pilih button berwarna hijau yang bertuliskan 'Code', kemudian pilih download zip
+
+2. ketika sudah mendownload atau clone, masuk kedalam folder 'workshop-unram-lorawan' menggunakan VS Code
+
+3. setelah VS Code terbuka, buka terminal di VS Code (letak button terminal ada disebelah atas dan pilih 'new terminal')
+
+4. ketik script berikut
+```bash
+npm install
+```
+
+5. kemudian setelah script diatas selesai, install nodemon (guna menjalankan aplikasi)
+```bash
+npm install --save-dev nodemon
+```
+
+6. kemudian duplicate env.example dan rename menjadi .env
+
+7. lalu jalankan aplikasi lewat terminal dengan ketik
+```bash
+npm run dev
+```
+(aplikasi berjalan di port 4000, jadi ketik di browser localhost:4000)
+
+## task
+dalam aplikasi ini teman-teman perlu melengkapi beberapa code didalam file antaresAPI.js
+
+1. lengkapi 3 fungi yang sudah diberikan mark (fill here) sesuai dengan materi yang disampaikan
--- service/antaresAPI.js
+++ service/antaresAPI.js
@@ -34,54 +34,15 @@ const getDataAntaresID = async () => {
 };
 
 const getDataAntaresBuzzer = async () => {
-  try {
-    const url = `https://platform.antares.id:8443/~/antares-cse/antares-id/${process.env.applicationName}/${process.env.deviceNameBuzzer}?fu=1&ty=4&drt=1`;
-    const result = await axios.get(url, {
-      headers: {
-        "X-M2M-Origin": process.env.accessKey,
-        Accept: "application/json",
-        "Content-Type": "application/json;ty=4",
-      },
-    });
-    return result.data;
-  } catch (e) {
-    console.log(e)
-    throw new Error("Error");
-  }
+  // fill here
 };
 
 const getDetailDataAntaresByUril = async (uril) => {
-  try {
-    const url = `https://platform.antares.id:8443/~${uril}`;
-    const result = await axios.get(url, {
-      headers: {
-        "X-M2M-Origin": process.env.accessKey,
-        Accept: "application/json",
-        "Content-Type": "application/json;ty=4",
-      },
-    });
-    return result.data;
-  } catch (e) {
-    console.log(e);
-    throw new Error("Error");
-  }
+  // fill here
 };
 
 const postData = async (data) => {
-  try {
-    const url = `https://platform.antares.id:8443/~/antares-cse/antares-id/${process.env.applicationName}/${process.env.deviceNameBuzzer}`;
-    const result = await axios.post(url, data, {
-      headers: {
-        "X-M2M-Origin": process.env.accessKey,
-        Accept: "application/json",
-        "Content-Type": "application/json;ty=4",
-      },
-    });
-
-    return result;
-  } catch (e) {
-    return e;
-  }
+  // fill here
 };
 module.exports = {
   getLatestData,
